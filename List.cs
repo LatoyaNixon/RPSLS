26eea82eb195e37f61e189f8f5423628f1d63114
LatoyaNixon <nixonlatoya.ln@gmail.com>
2021-11-16 12:21:24 -0500

    created a list of gestures

---
+++ List.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RPSLS
+{
+    internal class List<T>
+    {
+        internal void Add(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RPSLS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Here are the rules:
+            Rock crushes Scrissors,
+            Scissors cuts Paper,
+            Paper covers Rock,
+            Rock crushes Lizard,
+            Lizard poisons Spock,
+            Spock smashes Scissors,
+            Scissors decapitates Lizard,
+            Lizard eats Paper,
+            Paper disproves Spock,
+            Spock vaporizes Rock.*/
+
+
+
+
+
+            Console.WriteLine("Welcome RPSLS");
+            
+            
+        }
+    }
+}
